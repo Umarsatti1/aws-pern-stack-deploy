e8af324d07d3d012cc53c4458209a8a8674d9dc0
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-06-10 22:08:50 +0500

    Added docker

--- server/db.js
+++ server/db.js
@@ -40,11 +40,11 @@ async function initPool() {
 
   if (env === "development") {
     pool = new Pool({
-      user: "postgres",
-      password: "Sirsatti123!",
-      host: "localhost",
-      port: 5432,
-      database: "tododb"
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "Sirsatti123!",
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME || "tododb"
     });
   } else {
     const config = await getDBCredentialsFromSSM();
