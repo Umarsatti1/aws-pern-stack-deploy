ca7b415aefe07f6c847ab9e8d1f4559d52df4906
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-06-10 16:25:23 +0500

    Update db.js and index.js

--- server/db.js
+++ server/db.js
@@ -1,15 +1,16 @@
-const { SSM } = require('aws-sdk');
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+const AWS = require("aws-sdk");
 
-const ssm = new SSM();
+let pool;
 
-async function getParams() {
+async function getDBCredentialsFromSSM() {
+  const ssm = new AWS.SSM({ region: process.env.AWS_REGION || "us-east-1" });
   const names = [
-    '/pern/PGUSER',
-    '/pern/PGPASSWORD',
-    '/pern/PGHOST',
-    '/pern/PGPORT',
-    '/pern/PGDATABASE'
+    "/pern/DB_USER",
+    "/pern/DB_PASSWORD",
+    "/pern/DB_HOST",
+    "/pern/DB_NAME",
+    "/pern/DB_PORT"
   ];
 
   const params = {
@@ -18,18 +19,41 @@ async function getParams() {
   };
 
   const result = await ssm.getParameters(params).promise();
-
-  const paramMap = Object.fromEntries(
-    result.Parameters.map(p => [p.Name.split('/').pop(), p.Value])
-  );
-
-  return new Pool({
-    user: paramMap.PGUSER,
-    password: paramMap.PGPASSWORD,
-    host: paramMap.PGHOST,
-    port: parseInt(paramMap.PGPORT, 10),
-    database: paramMap.PGDATABASE
+  const paramMap = {};
+  result.Parameters.forEach(p => {
+    const key = p.Name.split("/").pop();
+    paramMap[key] = p.Value;
   });
+
+  return {
+    user: paramMap.DB_USER,
+    password: paramMap.DB_PASSWORD,
+    host: paramMap.DB_HOST,
+    port: parseInt(paramMap.DB_PORT, 10),
+    database: paramMap.DB_NAME
+  };
 }
 
-module.exports = getParams;
+async function initPool() {
+  const env = process.env.NODE_ENV || "development";
+
+  if (env === "development") {
+    pool = new Pool({
+      user: "postgres",
+      password: "Sirsatti123!",
+      host: "localhost",
+      port: 5432,
+      database: "tododb"
+    });
+  } else {
+    const config = await getDBCredentialsFromSSM();
+    pool = new Pool(config);
+  }
+}
+
+const poolPromise = (async () => {
+  await initPool();
+  return pool;
+})();
+
+module.exports = poolPromise;
