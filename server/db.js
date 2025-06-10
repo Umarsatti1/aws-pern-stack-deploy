3a25e1006c1bb702fdfe0cecc1a3557bb360393c
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-06-10 22:21:58 +0500

    Update .env and db.js

--- server/db.js
+++ server/db.js
@@ -41,7 +41,7 @@ async function initPool() {
   if (env === "development") {
     pool = new Pool({
       user: process.env.DB_USER || "postgres",
-      password: process.env.DB_PASSWORD || "Sirsatti123!",
+      password: process.env.DB_PASSWORD || "password123",
       host: process.env.DB_HOST || "localhost",
       port: parseInt(process.env.DB_PORT) || 5432,
       database: process.env.DB_NAME || "tododb"
