29a230b4db25b6dbb9e17ec14f39ae80de9a4f13
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-06-20 03:13:20 +0500

    Updaate deploy.yml and outputs.tf files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,18 @@ output "ecr_repo_urls" {
     frontend = module.ecr.frontend_repo_url
   }
 }
+
+output "ecs_task_role_arn" {
+  value       = module.iam.ecs_task_role_arn
+  description = "IAM Role ARN used by ECS Task"
+}
+
+output "private_subnet_ids" {
+  value       = module.vpc.private_subnets
+  description = "Private subnet IDs used by ECS service"
+}
+
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnets
+  description = "Public subnet IDs used by ALB"
+}
